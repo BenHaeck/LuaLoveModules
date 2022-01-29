e550f72b5406f686fb6c9592bffbda607a9ed65b
BenHaeck <84250841+BenHaeck@users.noreply.github.com>
2022-01-28 22:11:38 -0500

    Update MathFuncs.lua

--- Engine/MathFuncs.lua
+++ Engine/MathFuncs.lua
@@ -6,6 +6,14 @@ function MF.SmoothEdge (x,y)
 	return x * mult, y * mult;
 end
 
+MF.abs = math.abs;
+
+function MF.GetDir (x)
+	if x > 0 then return 1; end
+	if x < 0 then return -1; end
+	return 0;
+end
+
 function MF.Normalize (x,y)
 	if x == 0 or y == 0 then return 0,0; end
 	local mult = 1/math.sqrt ((x * x) + (y * y));
